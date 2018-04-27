ca92d9d119edbb33d63d8a28f10f3b216f217c4b
Niols <niols@niols.fr>
2018-04-27 17:52:34 +0200

    poc on int, string and tuples; need to use the PPX for PPXes

--- src/ppx_deriving_madcast.ml
+++ src/ppx_deriving_madcast.ml
@@ -1,10 +1,120 @@
+open Parsetree
+open Asttypes
+open Longident
+let mknoloc = Location.mknoloc
+
+module List = struct
+  include List
+
+  let mapi2 f al bl =
+    let rec mapi2 f i al bl =
+      match al , bl with
+      | [] , [] -> []
+      | a :: al' , b :: bl' -> (f i a b) :: mapi2 f (i+1) al' bl'
+      | _ -> assert false
+    in
+    mapi2 f 0 al bl
+end
+            
+exception CantCast of core_type * core_type * string
+
+let rec madcast itype otype =
+  match itype.ptyp_desc , otype.ptyp_desc with
+
+  (* string -> int *)
+    
+  | Ptyp_constr ({ txt = Lident "string" ; _ }, []) ,
+    Ptyp_constr ({ txt = Lident "int" ; _ }, []) ->
+     { pexp_desc = Pexp_ident (mknoloc (Lident "int_of_string")) ;
+       pexp_loc = Location.none ;
+       pexp_attributes = [] }
+
+  (* int -> string *)
+    
+  | Ptyp_constr ({ txt = Lident "int" ; _ }, []) ,
+    Ptyp_constr ({ txt = Lident "string" ; _ }, []) ->
+     { pexp_desc = Pexp_ident (mknoloc (Lident "string_of_int")) ;
+       pexp_loc = Location.none ;
+       pexp_attributes = [] }
+
+  (* tuple to tuple
+
+     check that they are of the same size and that we can cast each
+     components. example:
+
+         [%madcast: (string * int) -> (int * string)]
+
+     gives
+
+         (fun (c1,c2) -> (int_of_string c1, string_of_int c2)) *)
+    
+  | Ptyp_tuple itypes , Ptyp_tuple otypes ->
+     (
+       if List.length itypes = List.length otypes then
+         { pexp_desc =
+             Pexp_fun (
+                 Nolabel ,
+                 None ,
+                 (* function that matches a tuple (c1,c2,...,cn) as
+                    argument *)
+                 { ppat_desc = Ppat_tuple (
+                     List.mapi (fun i _ ->
+                       { ppat_desc =
+                           Ppat_var (mknoloc ("c"^(string_of_int i))) ;
+                         ppat_loc = Location.none ;
+                         ppat_attributes = [] })
+                       itypes
+                     ) ;
+                   ppat_loc = Location.none ;
+                   ppat_attributes = [] } ,
+                 (* and that return a tuple of the same size... *)
+                 { pexp_desc = Pexp_tuple (
+                     List.mapi2 (fun i isubtype osubtype ->
+                       { pexp_desc =
+                           (* ...and each component of the tuple is
+                              the madcast for the subtypes applied to
+                              the right argument *)
+                           Pexp_apply (
+                               madcast isubtype osubtype,
+                               [(Nolabel, { pexp_desc = Pexp_ident (mknoloc (Lident ("c"^(string_of_int i)))) ;
+                                            pexp_loc = Location.none ;
+                                            pexp_attributes = [] })]
+                             ) ;
+                         pexp_loc = Location.none ;
+                         pexp_attributes = [] })
+                       itypes otypes) ;
+                   pexp_loc = Location.none ;
+                   pexp_attributes = [] }
+               ) ;
+           pexp_loc = Location.none ;
+           pexp_attributes = [] }
+       else
+         raise (CantCast (itype, otype, "cannot cast tuples of different sizes"))
+     )
+    
+  | _ -> raise (CantCast (itype, otype, ""))
 
 (** When reading [\[%madcast: t\]], we call [core_type] on the type
    [t], and it returns an OCaml expression that will replace it. *)
-let core_type _ =
-  Format.eprintf "core_type@.";
-  assert false
+let core_type ptype =
+  match ptype.ptyp_desc with
+  | Ptyp_arrow (Asttypes.Nolabel, itype, otype) ->
+     (
+       try
+         madcast itype otype
+       with
+       | CantCast (itype, otype, reason) ->
+          Ppx_deriving.(
+           raise_errorf
+             "Cannot cast %s to %s: %s"
+             (string_of_core_type itype)
+             (string_of_core_type otype)
+             reason
+          )
+     )
+  | _ ->
+     failwith "madcast's type must be an arrow from the input type to the output type"
 
 (* Register the deriver "madcast" that only works on "core_type", that
-   is on inline statements [%madcast:] *) let () =
-   Ppx_deriving.(register (create "madcast" ~core_type ()))
+   is on inline statements [%madcast:] *)
+let () = Ppx_deriving.(register (create "madcast" ~core_type ()))
--- test/test_ppx_deriving_madcast.ml
+++ test/test_ppx_deriving_madcast.ml
@@ -1,5 +1,14 @@
 
+let () = Format.printf "%d@." ([%madcast: string -> int] "1")
+let () = Format.printf "%s@." ([%madcast: int -> string] 2)
+
 let () =
   let (a, b) = [%madcast: (string * int) -> (int * string)] ("1", 2) in
   Format.printf "(%d,%s)@." a b
 
+let () =
+  assert (
+      ([%madcast: (string * int) -> (int * string)]
+         ([%madcast: (int * string) -> (string * int)] (1, "2")))
+      = (1, "2")
+    )
