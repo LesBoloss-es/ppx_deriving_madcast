f82ac60825f4a4574a0f8a328a8a6c01fb134bd4
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2019-05-02 12:50:59 +0200

    fix ci

--- .travis.install.sh
+++ .travis.install.sh
@@ -6,13 +6,13 @@ fold_end () { printf 'travis_fold:end:%s\r' "$1"; }
 
 ## APT dependencies
 fold_start apt_deps 'Install APT dependencies...'
-sudo apt-get install ocaml aspcud
+sudo apt-get install ocaml
 fold_end apt_deps
 
 ## Install OPAM
 fold_start install_opam 'Install OPAM...'
 mkdir -p ~/.local/bin
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - \
+wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh -O - \
     | sh -s ~/.local/bin system
 export PATH=~/.local/bin:$PATH
 fold_end install_opam
@@ -24,7 +24,7 @@ if [ -z "$OCAML_VERSION" ]; then
     OCAML_VERSION=$DEFAULT_OCAML_VERSION
 fi
 opam switch "$OCAML_VERSION"
-eval $(opam config env)
+eval $(opam env)
 fold_end prep_opam
 
 ## OPAM dependencies
