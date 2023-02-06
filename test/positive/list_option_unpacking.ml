e5843aa7e8053e7341f3df3d6c123908a477f1f4
Niols <niols@niols.fr>
2023-02-06 17:38:28 +0100

    Even newer; fixing some more things

--- test/positive/list_option_unpacking.ml
+++ test/positive/list_option_unpacking.ml
@@ -7,9 +7,11 @@ let () =
 
 let () =
   let actual =
-    [|Some [|Some "1"; Some "Pierre"; Some "7"|];
-    Some [|Some "3"; Some "Paul"; None|];
-    None|]
+    [|
+      Some [|Some "1"; Some "Pierre"; Some "7"|];
+      Some [|Some "3"; Some "Paul"; None|];
+      None;
+    |]
     |> [%madcast: string option array option array -> (int * string * string option ) option list]
   in
   let expected =
